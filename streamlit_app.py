357290530e8942dd93eb17430f9ed4214ae0d323
Matt Khargie <mattkhargie@gmail.com>
2024-10-07 15:39:19 +0000

    Context

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,18 @@ client = OpenAI(api_key=openai_api_key)
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
 if "messages" not in st.session_state:
-    st.session_state.messages = [{'role':'system', 'content':"""
+    st.session_state.messages = []
+
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+
+
+context =  [{'role':'system', 'content':"""
                         You are a lawncare expert. Your job is to create a plan/schedule for lawncare, taking into account the weather, the type of grass, the soil temp, and any previous product applications.  I prefer Yard Mastery products. \
                         1. Retrieve the users history, including product application. If this is the first conversation with the user then ask about any applications done for the season so far and their dates. \
                         2. If the user provides a location, look up what type of grass lives in that location and use that information in your answer. \
@@ -64,13 +75,7 @@ if "messages" not in st.session_state:
                         <Application Instructions> """},    
                         {'role':'user', 'content':'I live in Oceanside,NY'}]
 
-# Display the existing chat messages via `st.chat_message`.
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
-# Create a chat input field to allow the user to enter a message. This will display
-# automatically at the bottom of the page.
 if prompt := st.chat_input("What is up?"):
 
     # Store and display the current prompt.
@@ -78,10 +83,12 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    context.append({'role':'user', 'content':prompt})
+
     # Generate a response using the OpenAI API.
     stream = client.chat.completions.create(
         model="gpt-4",
-        messages=[
+         messages=[
             {"role": m["role"], "content": m["content"]}
             for m in st.session_state.messages
         ],   
